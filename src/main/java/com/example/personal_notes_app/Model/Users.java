f4a160c8977728141cd02c514ff46d92b85ec3c8
Satyam Srivastav <satyam.srivastav363@gmail.com>
2024-10-09 07:15:29 +0530

    username of user if now unique

--- src/main/java/com/example/personal_notes_app/Model/Users.java
+++ src/main/java/com/example/personal_notes_app/Model/Users.java
@@ -3,6 +3,7 @@ package com.example.personal_notes_app.Model;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import lombok.Data;
 @Component
 @Document(collection="Users")
 public class Users {
+    @Indexed(unique=true)
     private String username;
     private String password;
     @Id
